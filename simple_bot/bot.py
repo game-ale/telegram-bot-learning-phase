2291b0911add9b1a6fc753d569f8b348cfde604b
Gemechu Alemu <alemugemechu44@gmail.com>
2026-02-03 22:09:37 +0300

    task is added

--- simple_bot/bot.py
+++ simple_bot/bot.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
